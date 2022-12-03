bca94db4173209189c5e80fd52a6e149ff08b933
Trisit Das <trisitdas@gmail.com>
2022-12-03 23:38:05 +0530

    all files done

---
+++ StockProject/stockproject/stockproject/asgi.py
@@ -0,0 +1,33 @@
+"""
+ASGI config for stockproject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+from channels.auth import AuthMiddlewareStack 
+from mainapp.routing import websocket_urlpatterns
+from urllib import URLRouter
+
+
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    # Just HTTP for now. (We can add other protocols later.)
+    "websocket" : AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
